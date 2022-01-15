ad42519473d8cc0676b7ac775f84526669092a12
NotANewt <meegafauna@gmail.com>
2022-01-14 20:06:09 -0600

    added rows for remaining hours, designed rows with bootstrap, added js to retrieve tasks from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,13 +11,21 @@ function saveTask() {
   localStorage.setItem(time, task);
 }
 
-// event listeners
-// show current date
-$("#currentDay").text(moment().format("MMMM Do, YYYY"));
-
 // retrieve tasks from local storage
 $("#hour9 .js-task").val(localStorage.getItem("hour9"));
 $("#hour10 .js-task").val(localStorage.getItem("hour10"));
+$("#hour11 .js-task").val(localStorage.getItem("hour11"));
+$("#hour12 .js-task").val(localStorage.getItem("hour12"));
+$("#hour13 .js-task").val(localStorage.getItem("hour13"));
+$("#hour14 .js-task").val(localStorage.getItem("hour14"));
+$("#hour15 .js-task").val(localStorage.getItem("hour15"));
+$("#hour16 .js-task").val(localStorage.getItem("hour16"));
+$("#hour17 .js-task").val(localStorage.getItem("hour17"));
+
+// event listeners
+
+// show current date
+$("#currentDay").text(moment().format("MMMM Do, YYYY"));
 
 // user clicks save button
 $(".js-saveBtn").on("click", saveTask);
